e441b3863ddccfd7e8dd8faa4617a270152ff9e9
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-08-20 20:39:37 -0300

    fix: ensure expect of await an create specification

--- src/modules/cars/useCases/createSpecification/createSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/createSpecificationUseCase.ts
@@ -20,7 +20,7 @@ class CreateSpecificationUseCase {
       throw new Error('Specification already exists!');
     }
 
-    this.specificationsRepository.create({ name, description });
+    await this.specificationsRepository.create({ name, description });
   }
 }
 
