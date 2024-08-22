f43a98fecaee676d50a82b731f0c1ef1f97556fa
Afranio Dantas <afranio.dantas@livsaude.com.br>
2024-08-22 14:48:34 -0300

    temp: saving progres

--- src/modules/accounts/repositories/iUsersRepository.ts
+++ src/modules/accounts/repositories/iUsersRepository.ts
@@ -2,6 +2,7 @@ import { ICreateUserDto } from '../dtos/iCreateUserDto';
 
 interface IUsersRepository {
   create(data: ICreateUserDto): Promise<void>;
+  findByEmail(email: string): Promise<void>;
 }
 
 export { IUsersRepository };
--- src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 import { IUsersRepository } from '../../repositories/iUsersRepository';
 import { ICreateUserDto } from '../../dtos/iCreateUserDto';
+import { hash } from 'bcrypt';
 
 @injectable()
 class CreateUserUseCase {
@@ -15,10 +16,18 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDto): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHash = await hash(password, 42);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
