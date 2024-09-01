72411ad6fe981f48d5de2b3d30c397d52804735e
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-09-01 11:59:00 -0300

    test: ensure prove creation category on success

---
+++ src/modules/cars/useCases/createCategory/createCategoryUseCase.spec.ts
@@ -0,0 +1,51 @@
+import 'reflect-metadata'; // necessário para uso de injeção de dependências com tsyringe
+import { CreateCategoryUseCase } from './createCategoryUseCase';
+import { ICategoriesRepository } from '../../repositories/iCategories.repository';
+import { AppError } from '../../../../errors/appError';
+
+//Arrange Values
+
+const makeRequest = {
+  name: 'Category Test',
+  description: 'Category description test',
+};
+
+const mockCategoriesRepository: jest.Mocked<ICategoriesRepository> = {
+  findByName: jest.fn(),
+  create: jest.fn(),
+  list: jest.fn(),
+};
+
+describe('CreateCategoryUseCase', () => {
+  let createCategoryUseCase: CreateCategoryUseCase;
+
+  beforeEach(() => {
+    createCategoryUseCase = new CreateCategoryUseCase(mockCategoriesRepository);
+  });
+
+  it('Should be able to create a new category', async () => {
+    //Arrange
+    mockCategoriesRepository.findByName.mockResolvedValueOnce(undefined);
+
+    //Act
+    await createCategoryUseCase.execute(makeRequest);
+
+    //Assert
+    expect(mockCategoriesRepository.create).toHaveBeenCalledWith(makeRequest);
+  });
+
+  it('Should not be able to create a new category with name exists', async () => {
+    //Arrange
+    mockCategoriesRepository.findByName.mockResolvedValueOnce({
+      id: '1',
+      name: 'Category Test',
+      description: 'Category description test',
+      created_at: new Date('02-02-2024'),
+    });
+
+    //Assert
+    await expect(createCategoryUseCase.execute(makeRequest)).rejects.toEqual(
+      new AppError('Category already exists!'),
+    );
+  });
+});
