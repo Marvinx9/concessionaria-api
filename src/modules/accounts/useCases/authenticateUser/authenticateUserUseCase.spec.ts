2ccfc57f6c0e416d189b953ee7d8509fe7e6682d
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-09-01 13:27:29 -0300

    test: ensure prove route authentication user on success

---
+++ src/modules/accounts/useCases/authenticateUser/authenticateUserUseCase.spec.ts
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import 'reflect-metadata'; // necessário para uso de injeção de dependências com tsyringe
+import { AppError } from '../../../../errors/appError';
+import { AuthenticateUserUseCase } from './authenticateUserUseCase';
+import { IUsersRepository } from '../../repositories/iUsersRepository';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+//Arrange Values
+const makeUser = {
+  id: '1',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+  driver_license: '23465',
+  is_admin: false,
+  avatar: 'dir_to_image',
+  created_at: new Date('24-02-2000'),
+};
+
+const makeValidCredential = {
+  email: 'any_email@mail.com',
+  password: 'any_password',
+};
+
+jest.mock('bcrypt', () => ({
+  compare: jest.fn(),
+}));
+
+jest.mock('jsonwebtoken', () => ({
+  sign: jest.fn(),
+}));
+
+const mockUsersRepository: jest.Mocked<IUsersRepository> = {
+  findByEmail: jest.fn(),
+  create: jest.fn(),
+  findById: jest.fn(),
+};
+
+describe('AuthenticateUserUseCase', () => {
+  let authenticateUserUseCase: AuthenticateUserUseCase;
+
+  beforeEach(() => {
+    authenticateUserUseCase = new AuthenticateUserUseCase(mockUsersRepository);
+  });
+
+  it('Should be able to authentication a user on success', async () => {
+    //Arrange
+    mockUsersRepository.findByEmail.mockResolvedValueOnce(makeUser);
+
+    (compare as jest.Mock).mockResolvedValue(true);
+
+    (sign as jest.Mock).mockReturnValue('fake-token');
+
+    //Act
+    const response = await authenticateUserUseCase.execute(makeValidCredential);
+
+    //Assert
+    expect(response).toHaveProperty('token', 'fake-token');
+    expect(response.user).toEqual({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+    });
+  });
+
+  it('Should throw if user not exists', async () => {
+    mockUsersRepository.findByEmail = jest.fn().mockResolvedValue(null);
+
+    await expect(
+      authenticateUserUseCase.execute(makeValidCredential),
+    ).rejects.toStrictEqual(new AppError('Email or password incorrect!', 400));
+  });
+
+  it('Should throw error if user insert invalid password', async () => {
+    mockUsersRepository.findByEmail = jest.fn().mockResolvedValue(makeUser);
+
+    (compare as jest.Mock).mockResolvedValue(false);
+
+    await expect(
+      authenticateUserUseCase.execute(makeUser),
+    ).rejects.toStrictEqual(new AppError('Email or password incorrect!', 400));
+  });
+});
