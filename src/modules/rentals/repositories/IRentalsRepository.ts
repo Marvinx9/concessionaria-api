55d4a8114b38289a7009e179cc56153daeea7744
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-09-10 21:13:08 -0300

    feat: create module create-rental on success

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,10 @@
+import { ICreateRentalDto } from '../dtos/ICreateRentalDto';
+import { Rental } from '../infra/typeorm/entities/rental';
+
+interface IRentalsRepository {
+  findOpenRentalByCar(car_id: string): Promise<Rental>;
+  findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDto): Promise<Rental>;
+}
+
+export { IRentalsRepository };
