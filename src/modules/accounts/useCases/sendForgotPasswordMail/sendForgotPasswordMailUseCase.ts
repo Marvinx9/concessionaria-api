5e108fa07d111df90939afc700cda1f5b7200a91
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-09-14 14:25:01 -0300

    temp: saving progres

--- src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailUseCase.ts
@@ -1,8 +1,46 @@
-// import { injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
+import { v4 as uuidV4 } from 'uuid';
+import { IUsersRepository } from '../../repositories/iUsersRepository';
+import { IUsersTokensRepository } from '../../repositories/iUsersTokensRepository';
+import { AppError } from '../../../../shared/errors/appError';
+import { IDateProvider } from '../../../../shared/container/providers/dateProvider/IDateProvider';
+import { IMailProvider } from '../../../../shared/container/providers/mailProvider/IMailProvider';
 
-// @injectable()
-// class SendForgotPasswordMailUseCase {
-//   async execute(email: string);
-// }
+@injectable()
+class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DayJsDateProvider')
+    private dateProvider: IDateProvider,
+    @inject('EtherealMailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+  async execute(email: string): Promise<void> {
+    const users = await this.usersRepository.findByEmail(email);
 
-// export { SendForgotPasswordMailUseCase };
+    if (!users) {
+      throw new AppError('User does not exists!');
+    }
+
+    const token = uuidV4();
+
+    const expires_date = this.dateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: users.id,
+      expires_date,
+    });
+
+    await this.mailProvider.sendMail(
+      email,
+      'Recuperação de senha',
+      `O link para o reset é ${token}`,
+    );
+  }
+}
+
+export { SendForgotPasswordMailUseCase };
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe';
 import { IDateProvider } from './dateProvider/IDateProvider';
 import { DayJsDateProvider } from './dateProvider/implementations/dayJsDateProvider';
+import { EtherealMailProvider } from './mailProvider/implementations/EtherealMailProvider';
+import { IMailProvider } from './mailProvider/IMailProvider';
 
 container.registerSingleton<IDateProvider>(
   'DayJsDateProvider',
   DayJsDateProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'EtherealMailProvider',
+  new EtherealMailProvider(),
 );
